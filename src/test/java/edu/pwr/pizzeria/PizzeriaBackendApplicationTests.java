7632ff49acfe3f2bc23a60f2c7ffba7828981a2d
Burdzi0 <burdzi0@programmer.net>
2020-03-30 08:47:17 +0200

    First commit

---
+++ src/test/java/edu/pwr/pizzeria/PizzeriaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pwr.pizzeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzeriaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

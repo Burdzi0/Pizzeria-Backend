bf90596378aa0dbffab1f389e13150bb61e36c9f
Burdzi0 <burdzi0@programmer.net>
2020-03-31 07:55:40 +0200

    Add TimeProvider, tests and AssertJ, closes #2.

--- src/main/java/edu/pwr/pizzeria/healthcheck/HealthCheckController.java
+++ src/main/java/edu/pwr/pizzeria/healthcheck/HealthCheckController.java
@@ -1,19 +1,23 @@
 package edu.pwr.pizzeria.healthcheck;
 
+import edu.pwr.pizzeria.common.TimeProvider;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 public class HealthCheckController {
 
+    private TimeProvider timeProvider;
+
+    public HealthCheckController(TimeProvider timeProvider) {
+        this.timeProvider = timeProvider;
+    }
+
     @GetMapping(value = "/v1/health-check", produces = APPLICATION_JSON_VALUE)
     public HealthCheck getStatus() {
         // TODO add database connection to check
-        return new HealthCheck(HealthStatus.HEALTHY, ZonedDateTime.now(ZoneId.of("UTC")));
+        return new HealthCheck(HealthStatus.HEALTHY, timeProvider.getTime());
     }
 }

30861f1af44aec45deed1f1a576fb7187b825ed7
Asia <asialuk98@gmail.com>
2020-04-05 14:17:03 +0200

    create models

---
+++ src/main/java/model/Ingredient.java
@@ -0,0 +1,89 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Entity
+public class Ingredient {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    private int quantity;
+    private BigDecimal price;
+    private boolean ifAllergen;
+
+    public Ingredient() {
+    }
+
+    public Ingredient(String name, int quantity, BigDecimal price, boolean ifAllergen) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+        this.ifAllergen = ifAllergen;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public boolean isIfAllergen() {
+        return ifAllergen;
+    }
+
+    public void setIfAllergen(boolean ifAllergen) {
+        this.ifAllergen = ifAllergen;
+    }
+
+    @Override
+    public String toString() {
+        return "Ingredient{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", quantity=" + quantity +
+                ", price=" + price +
+                ", ifAllergen=" + ifAllergen +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ingredient that = (Ingredient) o;
+        return id == that.id &&
+                quantity == that.quantity &&
+                ifAllergen == that.ifAllergen &&
+                name.equals(that.name) &&
+                price.equals(that.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, quantity, price, ifAllergen);
+    }
+}
---
+++ src/main/java/model/Pizza.java
@@ -0,0 +1,80 @@
+package model;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+public class Pizza {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String typeName;
+    @OneToMany
+    private List<Ingredient> ingredients;
+    private int diameter;
+    private PizzaCrust crust;
+
+    public Pizza() {
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public int getDiameter() {
+        return diameter;
+    }
+
+    public void setDiameter(int diameter) {
+        this.diameter = diameter;
+    }
+
+    public PizzaCrust getCrust() {
+        return crust;
+    }
+
+    public void setCrust(PizzaCrust crust) {
+        this.crust = crust;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pizza pizza = (Pizza) o;
+        return id == pizza.id &&
+                diameter == pizza.diameter &&
+                typeName.equals(pizza.typeName) &&
+                ingredients.equals(pizza.ingredients) &&
+                Objects.equals(crust, pizza.crust);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, typeName, ingredients, diameter, crust);
+    }
+
+    @Override
+    public String toString() {
+        return "Pizza{" +
+                "id=" + id +
+                ", typeName='" + typeName + '\'' +
+                ", ingredients=" + ingredients +
+                ", diameter=" + diameter +
+                ", crust='" + crust + '\'' +
+                '}';
+    }
+}

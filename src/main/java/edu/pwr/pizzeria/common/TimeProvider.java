38100e06fc5f0280eb18ab4269d017974820ae52
Burdzi0 <burdzi0@programmer.net>
2020-04-05 10:29:31 +0200

    Fix time provider - check on Heroku manually

--- src/main/java/edu/pwr/pizzeria/common/TimeProvider.java
+++ src/main/java/edu/pwr/pizzeria/common/TimeProvider.java
@@ -2,15 +2,17 @@ package edu.pwr.pizzeria.common;
 
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 @Component
 public class TimeProvider {
 
-    private final ZoneId zoneId = ZoneId.of("Europe/Warsaw");
+    public static final String EUROPE_WARSAW = "Europe/Warsaw";
 
     public ZonedDateTime getTime() {
-        return ZonedDateTime.now(zoneId);
+        final ZoneId zone = ZoneId.of(EUROPE_WARSAW);
+        return ZonedDateTime.of(LocalDateTime.now(), zone);
     }
 }

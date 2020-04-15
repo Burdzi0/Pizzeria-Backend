2efe7de8d7acd51d8b48baed1c672d65afb16efe
Asia <asialuk98@gmail.com>
2020-04-15 20:56:31 +0200

    add missing annotations

--- src/main/java/edu/pwr/pizzeria/service/IngredientService.java
+++ src/main/java/edu/pwr/pizzeria/service/IngredientService.java
@@ -2,10 +2,12 @@ package edu.pwr.pizzeria.service;
 
 import edu.pwr.pizzeria.model.dto.IngredientDto;
 import edu.pwr.pizzeria.repository.IngredientRepository;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class IngredientService {
 
     private IngredientRepository ingredientRepository;

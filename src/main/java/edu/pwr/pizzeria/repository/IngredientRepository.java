0c38fec01de8045e34b699758a2ae33cad9e4062
Asia <asialuk98@gmail.com>
2020-04-15 20:30:06 +0200

    fix unresolved conflict

--- src/main/java/edu/pwr/pizzeria/repository/IngredientRepository.java
+++ src/main/java/edu/pwr/pizzeria/repository/IngredientRepository.java
@@ -2,12 +2,6 @@ package edu.pwr.pizzeria.repository;
 
 import edu.pwr.pizzeria.model.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< HEAD
-import org.springframework.stereotype.Repository;
 
-@Repository
-=======
-
->>>>>>> origin/add-controllers-and-services
 public interface IngredientRepository extends JpaRepository<Ingredient, Integer> {
 }

9b8dd7ac63dc90c714cfc9fef0bf38d326ff4dc7
Asia <asialuk98@gmail.com>
2020-04-15 20:56:31 +0200

    add missing annotations

--- src/main/java/edu/pwr/pizzeria/controller/IngredientController.java
+++ src/main/java/edu/pwr/pizzeria/controller/IngredientController.java
@@ -4,8 +4,12 @@ import edu.pwr.pizzeria.model.dto.IngredientDto;
 import edu.pwr.pizzeria.service.IngredientService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
+@RestController
+@RequestMapping(value = "/v1/ingredient")
 public class IngredientController {
 
     private IngredientService ingredientService;

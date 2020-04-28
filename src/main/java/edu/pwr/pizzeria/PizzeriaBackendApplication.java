e628a8e5d3208bb1e2e168aa89b6dee798bae39f
Asia <asialuk98@gmail.com>
2020-04-28 13:18:23 +0200

    implement the RandomPizzaBuilder class

--- src/main/java/edu/pwr/pizzeria/PizzeriaBackendApplication.java
+++ src/main/java/edu/pwr/pizzeria/PizzeriaBackendApplication.java
@@ -1,5 +1,6 @@
 package edu.pwr.pizzeria;
 
+import edu.pwr.pizzeria.mock.RandomPizzaBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PizzeriaBackendApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(PizzeriaBackendApplication.class, args);
+        RandomPizzaBuilder rpb = new RandomPizzaBuilder();
+        rpb.initializeTestIngredients();
+        rpb.generateRandomPizza();
     }
 
 }

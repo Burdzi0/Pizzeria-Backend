5fcc400f95239f1f23e7a8a9c279ac1ccb04ce53
Asia <asialuk98@gmail.com>
2020-04-28 14:44:05 +0200

    move objects creation to DataStartup

--- src/main/java/edu/pwr/pizzeria/PizzeriaBackendApplication.java
+++ src/main/java/edu/pwr/pizzeria/PizzeriaBackendApplication.java
@@ -1,18 +1,10 @@
 package edu.pwr.pizzeria;
 
-import edu.pwr.pizzeria.mock.RandomPizzaBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PizzeriaBackendApplication {
 
-    public static void main(String[] args) {
-
-        SpringApplication.run(PizzeriaBackendApplication.class, args);
-        RandomPizzaBuilder rpb = new RandomPizzaBuilder();
-        rpb.initializeTestIngredients();
-        rpb.generateRandomPizza();
-    }
-
+    public static void main(String[] args) { SpringApplication.run(PizzeriaBackendApplication.class, args); }
 }
--- src/main/java/edu/pwr/pizzeria/config/DataStartup.java
+++ src/main/java/edu/pwr/pizzeria/config/DataStartup.java
@@ -1,5 +1,6 @@
 package edu.pwr.pizzeria.config;
 
+import edu.pwr.pizzeria.mock.RandomPizzaBuilder;
 import edu.pwr.pizzeria.model.Ingredient;
 import edu.pwr.pizzeria.model.Pizza;
 import edu.pwr.pizzeria.model.PizzaCrust;
@@ -35,5 +36,11 @@ public class DataStartup implements CommandLineRunner {
         pizza1.setPizzaIngredients(List.of(new PizzaIngredient(new Ingredient("Ham", BigDecimal.valueOf(4.5d), false), 1)));
         pizza1.setTypeName("Test1");
         pizzaRepository.save(pizza1);
+
+        RandomPizzaBuilder randomPizzaBuilder = new RandomPizzaBuilder();
+        randomPizzaBuilder.initializeTestIngredients();
+
+        Pizza pizza2 = randomPizzaBuilder.generateRandomPizza();
+        pizzaRepository.save(pizza2);
     }
 }

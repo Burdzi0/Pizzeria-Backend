67a3d416c1d51c90127a67f337b39e53cd422f88
Asia <asialuk98@gmail.com>
2020-05-12 18:34:06 +0200

    some changes
    implement the entity class
    
    add fields to the entity class
    
    make customer order an abstract class
    
    implement the CookViewOrderDto class
    
    implement the entity class
    
    add fields to the entity class
    
    implement the DeliveryViewOrder
    
    implement the DeliveryVireOrderDto

---
+++ src/main/java/edu/pwr/pizzeria/model/order/delivery/DeliveryOrderStatus.java
@@ -0,0 +1,6 @@
+package edu.pwr.pizzeria.model.order.delivery;
+
+public enum DeliveryOrderStatus {
+
+    OCZEKUJĄCE, W_DRODZE, DOSTARCZONE, ANULOWANE;
+}
--- src/main/java/edu/pwr/pizzeria/repository/PizzaRepository.java
+++ src/main/java/edu/pwr/pizzeria/repository/PizzaRepository.java
@@ -3,6 +3,5 @@ package edu.pwr.pizzeria.repository;
 import edu.pwr.pizzeria.model.pizza.Pizza;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
 }

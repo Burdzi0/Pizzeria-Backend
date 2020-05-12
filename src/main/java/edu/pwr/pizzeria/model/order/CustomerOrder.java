58254fe320216a77ba41973ee17306a533c898b0
Asia <asialuk98@gmail.com>
2020-05-12 11:33:49 +0200

    implement the entity class

--- src/main/java/edu/pwr/pizzeria/model/order/CustomerOrder.java
+++ src/main/java/edu/pwr/pizzeria/model/order/CustomerOrder.java
@@ -1,8 +1,12 @@
 package edu.pwr.pizzeria.model.order;
 
+import edu.pwr.pizzeria.model.pizza.Pizza;
 import edu.pwr.pizzeria.model.user.CustomerUser;
-
 import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
 
 @Entity
 public class CustomerOrder {
@@ -14,8 +18,11 @@ public class CustomerOrder {
     @ManyToOne
     private CustomerUser customerUser;
 
-    public CustomerOrder() {
-    }
+    @OneToMany
+    private List<Pizza> pizzas;
+    private BigDecimal total;
+    private Date date;
+    private OrderStatus status;
 
     public Long getId() {
         return id;
@@ -27,5 +34,71 @@ public class CustomerOrder {
 
     public CustomerUser getCustomerUser() {
         return customerUser;
+    }
+
+    public void setCustomerUser(CustomerUser customerUser) {
+        this.customerUser = customerUser;
+    }
+
+    public List<Pizza> getPizzas() {
+        return pizzas;
+    }
+
+    public void setPizzas(List<Pizza> pizzas) {
+        this.pizzas = pizzas;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CustomerOrder that = (CustomerOrder) o;
+        return id.equals(that.id) &&
+                customerUser.equals(that.customerUser) &&
+                pizzas.equals(that.pizzas) &&
+                Objects.equals(total, that.total) &&
+                date.equals(that.date) &&
+                status == that.status;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, customerUser, pizzas, total, date, status);
+    }
+
+    @Override
+    public String toString() {
+        return "CustomerOrder{" +
+                "id=" + id +
+                ", customerUser=" + customerUser +
+                ", pizzas=" + pizzas +
+                ", total=" + total +
+                ", date=" + date +
+                ", status=" + status +
+                '}';
     }
 }

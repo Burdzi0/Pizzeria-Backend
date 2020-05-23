7204fa52fedfab7f4c88267c601623adb8b99d45
Asia <asialuk98@gmail.com>
2020-05-23 23:31:31 +0200

    add service and controller

---
+++ src/main/java/edu/pwr/pizzeria/controller/CustomerOrderController.java
@@ -0,0 +1,31 @@
+package edu.pwr.pizzeria.controller;
+
+import edu.pwr.pizzeria.model.order.CustomerOrderDto;
+import edu.pwr.pizzeria.service.order.CustomerOrderService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/v1/order")
+public class CustomerOrderController {
+
+    private CustomerOrderService customerOrderService;
+
+    public CustomerOrderController(CustomerOrderService customerOrderService) {
+        this.customerOrderService = customerOrderService;
+    }
+
+    @GetMapping(value = "/{id}")
+    public CustomerOrderDto getPizza(@PathVariable Long id) {
+        return customerOrderService.getPizza(id);
+    }
+
+    @GetMapping
+    public List<CustomerOrderDto> getAll() {
+        return customerOrderService.getAll();
+    }
+}
--- src/main/java/edu/pwr/pizzeria/service/order/CustomerOrderService.java
+++ src/main/java/edu/pwr/pizzeria/service/order/CustomerOrderService.java
@@ -1,33 +1,41 @@
 package edu.pwr.pizzeria.service.order;
 
-import edu.pwr.pizzeria.model.order.dto.CookViewOrderDto;
-import edu.pwr.pizzeria.model.order.dto.DeliveryViewOrderDto;
-import edu.pwr.pizzeria.model.pizza.Pizza;
+import edu.pwr.pizzeria.model.order.CustomerOrderDto;
+import edu.pwr.pizzeria.model.pizza.dto.PizzaDto;
 import edu.pwr.pizzeria.repository.CustomerOrderRepository;
-import edu.pwr.pizzeria.repository.PizzaRepository;
+import edu.pwr.pizzeria.service.pizza.PizzaNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
 public class CustomerOrderService {
 
     private CustomerOrderRepository customerOrderRepository;
-    private PizzaRepository pizzaRepository;
 
     public CustomerOrderService(CustomerOrderRepository customerOrderRepository) {
         this.customerOrderRepository = customerOrderRepository;
     }
 
+    @Transactional(readOnly = true)
+    public CustomerOrderDto getPizza(Long id) {
+        return customerOrderRepository.findById(id)
+                .map(CustomerOrderDto::toDto)
+                .orElseThrow(() -> new PizzaNotFoundException("Pizza with id:" + id + " not found"));
+    }
+
+    @Transactional(readOnly = true)
+    public List<CustomerOrderDto> getAll() {
+        return customerOrderRepository.findAll()
+                .stream()
+                .map(CustomerOrderDto::toDto)
+                .collect(Collectors.toList());
+    }
+
     @Transactional
-    public void createOrder(CookViewOrderDto cookViewOrderDto) {
-//        final List<Pizza> pizzaList = cookViewOrderDto.getPizzas()
-//                .stream()
-//                .filter(Objects::nonNull)
-//                .map(pizzaId -> pizzaRepository.getOne(pizzaId))
-//                .collect(Collectors.toList());
+    public void changeStatus(){
+
     }
 }

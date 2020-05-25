0717fcd8eedfe483fc374054b2c4cbb47a08eec1
Asia <asialuk98@gmail.com>
2020-05-25 15:49:02 +0200

    fix previous changes

--- src/main/java/edu/pwr/pizzeria/controller/CustomerOrderController.java
+++ src/main/java/edu/pwr/pizzeria/controller/CustomerOrderController.java
@@ -1,8 +1,13 @@
 package edu.pwr.pizzeria.controller;
 
+import edu.pwr.pizzeria.model.order.dto.ChangeStatusRequestDto;
 import edu.pwr.pizzeria.service.order.CustomerOrderService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/v1/order")
@@ -14,5 +19,15 @@ public class CustomerOrderController {
         this.customerOrderService = customerOrderService;
     }
 
-
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "Status changed successfully"),
+            @ApiResponse(code = 400, message = "Suggested status doesn't follow the set order")
+    })
+    @ApiOperation(value = "Advance status", notes = "Advance the given status accordingly to the set order")
+    @PostMapping("/advance_order")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    @Transactional
+    public void requestOrderStatusChange(@RequestParam ChangeStatusRequestDto changeStatusRequestDto){
+        customerOrderService.advanceStatus(changeStatusRequestDto.getId(), changeStatusRequestDto.getNewOrderStatus());
+    }
 }
--- src/main/java/edu/pwr/pizzeria/service/order/CustomerOrderService.java
+++ src/main/java/edu/pwr/pizzeria/service/order/CustomerOrderService.java
@@ -3,15 +3,7 @@ package edu.pwr.pizzeria.service.order;
 import edu.pwr.pizzeria.model.order.CustomerOrder;
 import edu.pwr.pizzeria.model.order.CustomerOrderStatus;
 import edu.pwr.pizzeria.repository.CustomerOrderRepository;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Service
 public class CustomerOrderService {
@@ -22,17 +14,9 @@ public class CustomerOrderService {
         this.customerOrderRepository = customerOrderRepository;
     }
 
-    @ApiResponses({
-            @ApiResponse(code = 201, message = "Status changed successfully"),
-            @ApiResponse(code = 400, message = "Suggested status doesn't follow the set order")
-    })
-    @ApiOperation(value = "Advance status", notes = "Advance the given status accordingly to the set order")
-    @PostMapping("/advance_order")
-    @ResponseStatus(HttpStatus.ACCEPTED)
-    @Transactional
-    public void advanceStatus(@RequestParam Long id, @RequestParam CustomerOrderStatus newStatus) {
+    public void advanceStatus(Long id, CustomerOrderStatus newStatus) {
 
-        CustomerOrder customerOrder = customerOrderRepository.findById(id)
+        final CustomerOrder customerOrder = customerOrderRepository.findById(id)
                 .orElseThrow(() -> new CustomerOrderNotFoundException("CustomerOrder with id:" + id + " not found"));
 
         if (ifCorrectOrder(customerOrder.getStatus(), newStatus)) customerOrder.setStatus(newStatus);

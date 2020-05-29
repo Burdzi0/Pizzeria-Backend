c57bdefaf9e69f793ef0a6b96f01e2c9dfdef1c2
Asia <asialuk98@gmail.com>
2020-05-29 21:19:28 +0200

    fix issues, modify CustomerOrderController

--- src/main/java/edu/pwr/pizzeria/model/order/dto/CustomerOrderDto.java
+++ src/main/java/edu/pwr/pizzeria/model/order/dto/CustomerOrderDto.java
@@ -2,121 +2,45 @@ package edu.pwr.pizzeria.model.order.dto;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import edu.pwr.pizzeria.model.order.CustomerOrder;
-import edu.pwr.pizzeria.model.order.CustomerOrderStatus;
-import edu.pwr.pizzeria.model.pizza.Pizza;
-import edu.pwr.pizzeria.model.user.Address;
+import edu.pwr.pizzeria.model.pizza.dto.CustomPizzaDto;
+import edu.pwr.pizzeria.model.pizza.dto.StandardPizzaDto;
+import edu.pwr.pizzeria.model.user.AddressDto;
 
-import java.math.BigDecimal;
-import java.sql.Time;
-import java.util.Date;
 import java.util.List;
-import java.util.Objects;
 
 public class CustomerOrderDto {
 
-    private final Long id;
-    private final Address address;
-    private final List<Pizza> pizzas;
-    private final BigDecimal total;
-    private final Date date;
-    private final Time timePassed;
-    private final CustomerOrderStatus customerOrderStatus;
-    private final String notes;
+    private final List<StandardPizzaDto> pizzas;
+    private final List<CustomPizzaDto> customPizzaDtos;
+    private final AddressDto addressDto;
 
     @JsonCreator
-    public CustomerOrderDto(@JsonProperty("id") Long id,
-                            @JsonProperty("address") Address address,
-                            @JsonProperty("pizzas") List<Pizza> pizzas,
-                            @JsonProperty("total") BigDecimal total,
-                            @JsonProperty("date") Date date,
-                            @JsonProperty("time") Time timePassed,
-                            @JsonProperty("customOrderStatus") CustomerOrderStatus customerOrderStatus,
-                            @JsonProperty("notes") String notes) {
-        this.id = id;
-        this.address = address;
+    public CustomerOrderDto(@JsonProperty("standard") List<StandardPizzaDto> pizzas,
+                            @JsonProperty("customs") List<CustomPizzaDto> customPizzaDtos,
+                            @JsonProperty("address") AddressDto addressDto) {
         this.pizzas = pizzas;
-        this.total = total;
-        this.date = date;
-        this.timePassed = timePassed;
-        this.customerOrderStatus = customerOrderStatus;
-        this.notes = notes;
+        this.customPizzaDtos = customPizzaDtos;
+        this.addressDto = addressDto;
     }
 
-    public static CustomerOrderDto toDto(CustomerOrder customerOrder) {
-        return new CustomerOrderDto(customerOrder.getId(),
-                customerOrder.getAddress(),
-                customerOrder.getPizzas(),
-                customerOrder.getTotal(),
-                customerOrder.getDate(),
-                customerOrder.getTimePassed(),
-                customerOrder.getStatus(),
-                customerOrder.getNotes());
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public List<Pizza> getPizzas() {
+    public List<StandardPizzaDto> getPizzas() {
         return pizzas;
     }
 
-    public BigDecimal getTotal() {
-        return total;
+    public List<CustomPizzaDto> getCustomPizzaDtos() {
+        return customPizzaDtos;
     }
 
-    public Date getDate() {
-        return date;
-    }
-
-    public Time getTimePassed() {
-        return timePassed;
-    }
-
-    public CustomerOrderStatus getCustomerOrderStatus() {
-        return customerOrderStatus;
-    }
-
-    public String getNotes() {
-        return notes;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        CustomerOrderDto that = (CustomerOrderDto) o;
-        return id.equals(that.id) &&
-                address.equals(that.address) &&
-                pizzas.equals(that.pizzas) &&
-                Objects.equals(total, that.total) &&
-                Objects.equals(date, that.date) &&
-                Objects.equals(timePassed, that.timePassed) &&
-                customerOrderStatus == that.customerOrderStatus &&
-                Objects.equals(notes, that.notes);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, address, pizzas, total, date, timePassed, customerOrderStatus, notes);
+    public AddressDto getAddressDto() {
+        return addressDto;
     }
 
     @Override
     public String toString() {
         return "CustomerOrderDto{" +
-                "id=" + id +
-                ", address=" + address +
-                ", pizzas=" + pizzas +
-                ", total=" + total +
-                ", date=" + date +
-                ", timePassed=" + timePassed +
-                ", customerOrderStatus=" + customerOrderStatus +
-                ", notes='" + notes + '\'' +
+                "pizzas=" + pizzas +
+                ", customPizzaDtos=" + customPizzaDtos +
+                ", addressDto=" + addressDto +
                 '}';
     }
 }
---
+++ src/main/java/edu/pwr/pizzeria/model/pizza/dto/StandardPizzaDto.java
@@ -0,0 +1,25 @@
+package edu.pwr.pizzeria.model.pizza.dto;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class StandardPizzaDto {
+
+    private final int id;
+    private final int diameter;
+
+    @JsonCreator
+    public StandardPizzaDto(@JsonProperty("id") int id,
+                            @JsonProperty("diameter") int diameter){
+        this.id = id;
+        this.diameter = diameter;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getDiameter() {
+        return diameter;
+    }
+}

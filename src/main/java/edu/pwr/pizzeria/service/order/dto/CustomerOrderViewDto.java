f97cb6f0bded514e5083cd867512c6b325eee0f1
Burdzi0 <burdzi0@programmer.net>
2020-06-08 21:12:31 +0200

    Add price and address

--- src/main/java/edu/pwr/pizzeria/service/order/dto/CustomerOrderViewDto.java
+++ src/main/java/edu/pwr/pizzeria/service/order/dto/CustomerOrderViewDto.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import edu.pwr.pizzeria.model.order.CustomerOrder;
 import edu.pwr.pizzeria.model.order.CustomerOrderStatus;
+import edu.pwr.pizzeria.model.user.Address;
+import edu.pwr.pizzeria.model.user.AddressDto;
 
 import java.time.Instant;
 import java.util.List;
@@ -16,16 +18,22 @@ public class CustomerOrderViewDto {
     private Instant date;
     private List<PizzaContentDto> pizzas;
     private CustomerOrderStatus status;
+    private AddressDto address;
+    private double price;
 
     @JsonCreator
     public CustomerOrderViewDto(@JsonProperty("orderId") long orderId,
                                 @JsonProperty("date") Instant date,
                                 @JsonProperty("pizzas") List<PizzaContentDto> pizzas,
-                                @JsonProperty("status") CustomerOrderStatus status) {
+                                @JsonProperty("status") CustomerOrderStatus status,
+                                @JsonProperty("address") AddressDto address,
+                                @JsonProperty("price") double price) {
         this.orderId = orderId;
         this.date = date;
         this.pizzas = pizzas;
         this.status = status;
+        this.address = address;
+        this.price = price;
     }
 
     public static CustomerOrderViewDto toDto(CustomerOrder customerOrder) {
@@ -37,7 +45,7 @@ public class CustomerOrderViewDto {
                         .map(PizzaContentDto::toDto))
                 .collect(Collectors.toUnmodifiableList());
 
-        return new CustomerOrderViewDto(customerOrder.getId(), customerOrder.getDate(), contents, customerOrder.getStatus());
+        return new CustomerOrderViewDto(customerOrder.getId(), customerOrder.getDate(), contents, customerOrder.getStatus(), AddressDto.toDto(customerOrder.getAddress()), customerOrder.getTotal().doubleValue());
     }
 
     public long getOrderId() {
@@ -54,5 +62,13 @@ public class CustomerOrderViewDto {
 
     public CustomerOrderStatus getStatus() {
         return status;
+    }
+
+    public AddressDto getAddress() {
+        return address;
+    }
+
+    public double getPrice() {
+        return price;
     }
 }

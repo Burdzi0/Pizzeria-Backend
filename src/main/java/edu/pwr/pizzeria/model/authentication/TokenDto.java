5c28c3b7ea23655c6120b93597796c32cbdf05b4
Burdzi0 <burdzi0@programmer.net>
2020-06-08 21:03:14 +0200

    Add roles

--- src/main/java/edu/pwr/pizzeria/model/authentication/TokenDto.java
+++ src/main/java/edu/pwr/pizzeria/model/authentication/TokenDto.java
@@ -2,17 +2,26 @@ package edu.pwr.pizzeria.model.authentication;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
 
 public class TokenDto {
 
     private final String token;
+    private final Collection<? extends GrantedAuthority> roles;
 
     @JsonCreator
-    public TokenDto(@JsonProperty("token") String token) {
+    public TokenDto(@JsonProperty("token") String token, @JsonProperty("roles") Collection<? extends GrantedAuthority> roles) {
         this.token = token;
+        this.roles = roles;
     }
 
     public String getToken() {
         return token;
+    }
+
+    public Collection<? extends GrantedAuthority> getRoles() {
+        return roles;
     }
 }
